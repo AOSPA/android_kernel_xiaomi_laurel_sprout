33fb19c4395924b467c524c1c07b907037b28833
Ethan Chen <intervigil@gmail.com>
2015-02-27 18:31:48 -0800

    power: reset: Allow device to preserve memory on restart
    * Always perform a soft reset as this preserves memory contents,
      including pstore and other persistent memory.
    
    Change-Id: Ideca44c9f38d2ebcd437ff289f8e036922eafcd2

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -276,6 +276,10 @@ static void msm_restart_prepare(const char *cmd)
 
 	if (force_warm_reboot)
 		pr_info("Forcing a warm reset of the system\n");
+		
+#ifdef CONFIG_QCOM_PRESERVE_MEM
+	need_warm_reset = true;
+#endif
 
 	/* Hard reset the PMIC unless memory contents must be maintained. */
 	if (force_warm_reboot || need_warm_reset)
