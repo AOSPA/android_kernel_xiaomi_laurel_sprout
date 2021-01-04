afa718d75930d2fb580889f171634403a82185c9
Vishalcj17 <vishalcj@aospa.co>
2021-01-04 16:55:05 +0530

    techpack: max98937: Gaurd max989xx_calib_save with DEBUG_FS flags.
    - This function is unused.
    
    - Also move *_RDC definitions out of debug code.
    
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I37c3626170a72da5ede8718908fd5f042a984d0a

--- techpack/audio/asoc/codecs/max98937.c
+++ techpack/audio/asoc/codecs/max98937.c
@@ -571,6 +571,7 @@ struct param_info {
 	char name[80];
 	int q_val;
 };
+#endif
 
 //MULTIPLE = 3.33,  rdc/(1<<27) * MULTIPLE = [min, max] ohm
 //
@@ -592,7 +593,6 @@ struct param_info {
 #define EAR_RDC_MAX  (1370391217) //34 / 3.33 * (1<<27)
 #define EAR_RDC_DEFAULT (1168863097)  // 29 / 3.33 * 134217728
 */
-#endif
 
 static struct {
 	bool l_calib_stat;
@@ -775,7 +775,8 @@ static int max989xx_calib_get(uint32_t *calib_value, int ch)
 	return found;
 }
 
-static int max989xx_calib_save(uint32_t calib_value, int ch)
+#ifdef CONFIG_DEBUG_FS
+static int max989xx_calib_save (uint32_t calib_value, int ch)
 {
 	struct file *pfile = NULL;
 	mm_segment_t old_fs;
@@ -810,6 +811,7 @@ static int max989xx_calib_save(uint32_t calib_value, int ch)
 
 	return ret;
 }
+#endif
 
 static inline bool rdc_check_valid(uint32_t rdc, int ch)
 {
